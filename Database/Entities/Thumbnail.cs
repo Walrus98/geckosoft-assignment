5db8d9d33f39fb2eb9124a71083b56e8b90751c9
Walrus <samuele.calugi@gmail.com>
2024-08-06 12:13:33 +0200

    feat(be): implement thumbnail status

--- Database/Entities/Thumbnail.cs
+++ Database/Entities/Thumbnail.cs
@@ -1,9 +1,18 @@
 namespace VideoMaker.Database.Entities;
 
 public class Thumbnail {
+
+    public enum ThumbnailStatus {
+        QUEUED,
+        ANALYZING,
+        COMPLETED,
+        FAILED,
+    }
+
     public Guid Id { get; set; }
     public Guid VideoId { get; set; }
     public int Width { get; set; }
     public int Height { get; set; }
     public required string FilePath { get; set; }
+    public ThumbnailStatus Status { get; set; }
 }
